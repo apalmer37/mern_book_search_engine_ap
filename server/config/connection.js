cd9adddba88101039eaad1f7670612768ad7d277
Alex Palmer <alexpalmer37@gmail.com>
2021-11-08 01:07:17 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
   {
@@ -8,3 +10,4 @@ mongoose.connect(
   },
 );
 
+module.exports = mongoose.connection;
