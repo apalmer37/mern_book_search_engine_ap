8de42fc450bf243ff641caf18bf350b446739e9b
Alex Palmer <alexpalmer37@gmail.com>
2021-11-08 01:13:25 -0500

    cleaned up mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,16 @@
 import { gql } from '@apollo/client';
 
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -11,18 +22,6 @@ export const LOGIN_USER = gql`
     }
   }
 `;
-export const CREATE_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    createUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
-
 export const SAVE_BOOK = gql`
   mutation saveBook($input: BookInput!) {
     saveBook(input: $input) {
@@ -51,7 +50,7 @@ export const DELETE_BOOK = gql`
             description
             title
             image
-            link    
+            link
       }
     }
   }
